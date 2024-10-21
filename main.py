4ada9cbd8ef35150d345f072185e683de7f8e2e8
Idrisvohra9 <idrishaider987@gmail.com>
2024-10-21 16:54:03 +0530

    Done for now

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 import yt_dlp
 from tqdm import tqdm
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import os
+import subprocess
+import threading
+import sys
 
+
+def resource_path(relative_path):
+    """ Get the absolute path to the resource, works for PyInstaller. """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+class ProgressUpdater:
+    def __init__(self):
+        self.pbar = None
+
+    def hook(self, d):
+        if d["status"] == "downloading":
+            if self.pbar is None:
+                file_size = d.get("total_bytes", 0)
+                if file_size > 0:  # Ensure file size is valid
+                    self.pbar = tqdm(
+                        total=file_size, unit="B", unit_scale=True, desc=d["filename"]
+                    )
+            if self.pbar is not None:  # Ensure pbar is initialized before using
+                downloaded = d.get("downloaded_bytes", 0)
+                self.pbar.n = downloaded
+                self.pbar.refresh()
+        elif d["status"] == "finished":
+            if self.pbar:
+                self.pbar.close()
+                self.pbar = None
 
 def download_youtube_audio(video_url):
     try:
-
-        def tqdm_progress_hook(d):
-            if d["status"] == "downloading":
-                file_size = d.get("total_bytes", 0)
-                progress = tqdm(
-                    total=file_size, unit="B", unit_scale=True, desc=d["filename"]
-                )
-                progress.update(d.get("downloaded_bytes", 0))
+        progress_updater = ProgressUpdater()
 
         ydl_opts = {
             "format": "bestaudio/best",
@@ -23,18 +54,98 @@ def download_youtube_audio(video_url):
                 }
             ],
             "outtmpl": "music/%(title)s.%(ext)s",
-            "progress_hooks": [tqdm_progress_hook],
+            "progress_hooks": [progress_updater.hook],
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([video_url])
+            info = ydl.extract_info(video_url, download=True)  # Change this to True
+            filename = ydl.prepare_filename(info)
 
-        print("Downloaded successfully")
+        file_path = os.path.join(
+            "music", f"{os.path.splitext(os.path.basename(filename))[0]}.mp3"
+        )
+
+        def open_file():
+            if os.name == "nt":  # For Windows
+                os.startfile(file_path)
+            elif os.name == "posix":  # For macOS and Linux
+                subprocess.call(("open", file_path))
+
+        status_label.config(text="Download completed!")
+        root.update()
+        messagebox.showinfo(
+            "Success",
+            "Video downloaded and converted successfully! Click OK to open the file.",
+            icon=messagebox.INFO,
+        )
+        open_file()
+
     except Exception as e:
-        print(f"Error occurred: {str(e)}")
+        messagebox.showerror("Error", f"Error occurred: {str(e)}")
 
 
-# Example usage
+def on_download():
+    video_url = url_entry.get()
+
+    # Run the download in a separate thread to prevent UI freezing
+    download_thread = threading.Thread(target=download_youtube_audio, args=(video_url,))
+    download_thread.start()
+    status_label.config(text="Download started...")
+
+
+# Create the main window
+root = tk.Tk()
+root.title("MP3 Harvest")
+root.geometry("400x540")
+
+# Set the icon using resource_path
+root.iconbitmap(resource_path("assets/icon.ico"))
+
+root.configure(bg="#2E2E2E")
+
+style = ttk.Style()
+style.theme_use("clam")
+style.configure("TLabel", foreground="white", background="#2E2E2E")
+style.configure("TEntry", fieldbackground="#3E3E3E", foreground="white")
+style.configure("TButton", background="#3E3E3E", foreground="white")
+
+# Create and pack widgets
+img = tk.PhotoImage(file=resource_path("assets/does.png"))
+img = img.subsample(2, 2)  # Resize the image to half its original size
+label = tk.Label(root, image=img, background="#2E2E2E", justify="center")
+label.pack()
+
+url_label = ttk.Label(root, text="Enter YouTube URL:")
+url_label.pack(pady=10)
+url_entry = ttk.Entry(root, width=50)
+url_entry.pack()
+
+download_button = ttk.Button(root, text="Download", command=on_download)
+download_button.pack(pady=20)
+
+status_label = ttk.Label(root, text="")
+status_label.pack()
+
+img2 = tk.PhotoImage(file=resource_path("assets/ta.png"))
+img2 = img2.subsample(2, 2)  # Resize the image to half its original size
+
+
+def open_link(event):
+    import webbrowser
+
+    webbrowser.open("https://www.youtube.com/@TechAchievers-IV/featured")
+
+
+label2 = tk.Label(
+    root,
+    image=img2,
+    background="#2E2E2E",
+    justify="center",
+    compound="center",
+    cursor="hand2",
+)
+label2.pack(pady=10)
+label2.bind("<Button-1>", open_link)
+
 if __name__ == "__main__":
-    video_url = input("Enter the YouTube video URL: ")
-    download_youtube_audio(video_url)
+    root.mainloop()
